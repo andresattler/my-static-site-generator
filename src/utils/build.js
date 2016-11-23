164416368eff7dd1d7016a0a257b16f7af855cdf
andresattler <andresattler.webdev@gmail.com>
2016-11-23 10:28:24 -0400

    added cleaning of the publish directory

--- src/utils/build.js
+++ src/utils/build.js
@@ -1,11 +1,13 @@
 import path from 'path';
 import createRoutes from './createRoutes';
 import buildHTML from './buildHTML';
+import del from 'del';
 function build(){
+  del.sync('publish/*');
   const dir = path.resolve('.');
   const paths = createRoutes(dir);
   paths.forEach(function(path){
-    buildHTML(path);
+    buildHTML(path, dir);
   })
 };
 
