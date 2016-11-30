8aae65a6af119d9b28116f5ad0520136197a222c
andresattler <andresattler.webdev@gmail.com>
2016-11-30 14:51:08 -0400

    added live server for development

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import program from 'commander';
 import build from '../utils/build';
-
+import develop from '../utils/develop';
 program
   .option('-b, build', 'Builds the html')
+  .option('-d, develop', 'Starts Development Server')
   .parse(process.argv);
 
 
 if(program.build) build();
+if(program.develop) develop();
---
+++ src/utils/develop.js
@@ -0,0 +1,20 @@
+import liveServer from 'live-server';
+import http from 'http';
+import watch from 'node-watch';
+import build from './build'
+
+function develop() {
+  build();
+  watch(['html.js', './pages', './themes'], function(){
+    console.log('rebuilding');
+    build();
+  });
+
+  const params = {
+    port: 8000,
+    root: "./publish",
+  };
+  liveServer.start(params);
+}
+
+export default develop;
